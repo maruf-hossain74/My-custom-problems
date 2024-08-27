d100979864a1d17a497b195be6bdb167bcaa60de
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2024-08-27 23:19:27 +0600

    800 Rating Codeforces standard

---
+++ Maruf and the speedy supergirl.cpp
@@ -0,0 +1,16 @@
+//Problem statement: https://codeforces.com/gym/538334/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    //ios::sync_with_stdio(0),cin.tie(0),cout.tie(0),cin >> t;
+    cin >> t;
+    while(t--) {
+        double N, M = 10000;
+        cin>> N;
+        N *= 1.25;
+        cout << min({M, N}) << endl;
+    }
+    return 0;
+}
